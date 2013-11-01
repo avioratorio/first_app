d1df17fbfa6fcc02b5d51005fc0a39e9de2c35a5
Knoppix User <knoppix@Microknoppix.(none)>
2013-11-01 18:35:22 -0400

    Initialize first_app repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'd90526e5fdcaa63e1f52c7ff47690e15dc348b0ff10e5a205bac53d762bbf544871d303e1ec7ca3df3eed121ba8ea3203b8cbaa8e085ca16e7813aae924eff09'
